125c9b0e947118c8bc8b67ec3663c335c9b6b03f
Mouhamed Al Amine Gueye <alamine2902@gmail.com>
2023-02-07 18:22:03 +0000

    07/02/2023 Dynamic classes list added

---
+++ gesSchoolApp/src/com/gesschoolapp/view/ClassItemController.java
@@ -0,0 +1,46 @@
+package com.gesschoolapp.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClassItemController implements Initializable {
+
+    @FXML
+    private ImageView classImg;
+
+    @FXML
+    private Label labelAnnee;
+
+    @FXML
+    private Label labelFormation;
+
+    @FXML
+    private Label labelIntitule;
+
+    @FXML
+    private HBox classCard;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void setData(String i, String f, String a){
+        labelIntitule.setText(i);
+        labelFormation.setText(f);
+        labelAnnee.setText(a);
+    }
+
+    @FXML
+    void onClickClass(MouseEvent event) {
+        System.out.println("CLICKED");
+    }
+}
