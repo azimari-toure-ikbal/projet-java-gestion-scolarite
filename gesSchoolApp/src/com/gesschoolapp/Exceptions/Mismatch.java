73a017f0fa9b6dcb5da6b91149ef3642d03c0c34
Ikbal AZIMARI TOURE <ikbal@Ikbal-mbp.local>
2023-02-16 15:57:56 +0000

    NotesCSV avec verifications supplémentaires. Mismatch exception

---
+++ gesSchoolApp/src/com/gesschoolapp/Exceptions/Mismatch.java
@@ -0,0 +1,7 @@
+package com.gesschoolapp.Exceptions;
+
+public class Mismatch extends Exception {
+    public Mismatch(String message) {
+        super(message);
+    }
+}
--- gesSchoolApp/src/com/gesschoolapp/gescsv/NotesCSV.java
+++ gesSchoolApp/src/com/gesschoolapp/gescsv/NotesCSV.java
@@ -2,10 +2,13 @@ package com.gesschoolapp.gescsv;
 
 import com.gesschoolapp.Exceptions.CSVException;
 import com.gesschoolapp.Exceptions.DAOException;
+import com.gesschoolapp.Exceptions.Mismatch;
 import com.gesschoolapp.db.DAOClassesImpl.ApprenantDAOImp;
 import com.gesschoolapp.db.DAOClassesImpl.ModuleDAOImp;
 import com.gesschoolapp.db.DAOClassesImpl.NoteDAOImp;
 import com.gesschoolapp.gescsv.reader.CSVReader;
+import com.gesschoolapp.models.classroom.Classe;
+import com.gesschoolapp.models.matieres.Module;
 import com.gesschoolapp.models.matieres.Note;
 
 import java.io.BufferedReader;
@@ -14,6 +17,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class NotesCSV implements CSVReader<Note> {
     @Override
@@ -69,7 +73,6 @@ public class NotesCSV implements CSVReader<Note> {
         List<Note> notes = new ArrayList<>();
 
         for (String[] line : data) {
-            //Note has id, note(int), apprenant(Apprenant), module(Module), semestre(int)
             if (line.length != 4) {
                 throw new CSVException("Le fichier n'est pas au bon format");
             }
@@ -82,7 +85,73 @@ public class NotesCSV implements CSVReader<Note> {
             }
             note.setModule(line[2]);
             note.setNote(Integer.parseInt(line[3]));
-            note.setSemestre(1);
+
+            // Create the note
+            try {
+                noteDAOImp.create(note);
+            } catch (DAOException e) {
+                throw new CSVException("Une erreur est survenue lors de la création de la note : " + e.getMessage());
+            }
+        }
+        return notes;
+    }
+
+    /**
+     * @param data
+     * @param module
+     * @param classse
+     * @return List
+     * @throws CSVException
+     * @throws Mismatch
+     */
+    public List<Note> csvToObject(List<String[]> data, Module module, Classe classse) throws CSVException, Mismatch {
+        // Verify if the list is not empty
+        if (data.isEmpty()) {
+            throw new CSVException("La liste est vide");
+        }
+
+        // Verify if the class exists
+        if (classse == null) {
+            throw new CSVException("La classe n'existe pas");
+        }
+
+        NoteDAOImp noteDAOImp = new NoteDAOImp();
+        ApprenantDAOImp apprenantDAOImp = new ApprenantDAOImp();
+        ModuleDAOImp moduleDAOImp = new ModuleDAOImp();
+
+        List<Note> notes = new ArrayList<>();
+
+        for (String[] line : data) {
+            if (line.length != 4) {
+                throw new CSVException("Le fichier n'est pas au bon format");
+            }
+
+            // Verify if the student exists
+            try {
+                if (apprenantDAOImp.searchByMatricule(Integer.parseInt(line[0])) == null) {
+                    throw new CSVException("L'apprenant n'existe pas");
+                }
+            } catch (DAOException e) {
+                throw new CSVException("Une erreur est survenue lors de la recherche de l'apprenant : " + e.getMessage());
+            }
+
+            // Verify if the student is in the class
+            try {
+                if (!Objects.equals(apprenantDAOImp.searchByMatricule(Integer.parseInt(line[0])).getClasse(), classse.getIntitule())) {
+                    throw new Mismatch("L'apprenant n'est pas dans la classe");
+                }
+            } catch (DAOException e) {
+                throw new CSVException("Une erreur est survenue lors de la recherche de l'apprenant : " + e.getMessage());
+            }
+
+            Note note = new Note();
+            try {
+                note.setApprenant(apprenantDAOImp.searchByMatricule(Integer.parseInt(line[0])));
+            } catch (DAOException e) {
+                throw new CSVException("Une erreur est survenue lors de la recherche de l'apprenant : " + e.getMessage());
+            }
+            note.setModule(module.getIntitule());
+            note.setNote(Integer.parseInt(line[3]));
 
             // Create the note
             try {
