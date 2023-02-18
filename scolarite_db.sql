999c30ddb1b2766e12a61e1d17695d03f9ceb6a1
Mouhamed Lawal Dan Azoumi <82711872+law-san-667@users.noreply.github.com>
2023-02-18 21:13:37 +0000

    modules added

--- scolarite_db.sql
+++ scolarite_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 16, 2023 at 01:02 PM
+-- Generation Time: Feb 18, 2023 at 09:12 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -39,22 +39,7 @@ CREATE TABLE IF NOT EXISTS `apprenants` (
   `matricule` int(11) NOT NULL,
   PRIMARY KEY (`idApprenant`),
   UNIQUE KEY `matricule` (`matricule`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `apprenants`
---
-
-INSERT INTO `apprenants` (`idApprenant`, `prenom`, `nom`, `dtNaiss`, `nationalite`, `echeancier`, `sexe`, `matricule`) VALUES
-(1, 'Moussa', 'DIOP', '2010-02-15', 'sénégalais', 0, 'M', 100),
-(2, 'Max', 'BIRD', '2010-06-13', 'français', 0, 'M', 101),
-(3, 'Marie', 'PLASSARD', '2010-01-16', 'belge', 0, 'F', 102),
-(4, 'Fabienne', 'DUPONT', '2010-08-11', 'française', 0, 'F', 103),
-(16, 'Axel', 'Zagadou', '2009-10-05', 'Camerounais', 0, 'M', 104),
-(17, 'Warren', 'Emery', '2009-10-05', 'Français', 0, 'M', 105),
-(18, 'Warren', 'Emery', '2009-10-05', 'Français', 0, 'M', 106),
-(19, 'Nathaniel', 'CLYNE', '2009-11-13', 'Anglaise', 0, 'M', 107),
-(20, 'Brice', 'CALAN', '2018-02-23', 'Suisse', 0, 'M', 108);
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -83,22 +68,7 @@ CREATE TABLE IF NOT EXISTS `classeapprenant` (
   PRIMARY KEY (`idClasseAppenant`),
   KEY `classeapprenant_ibfk_1` (`idApprenant`),
   KEY `classeapprenant_ibfk_2` (`idClasse`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `classeapprenant`
---
-
-INSERT INTO `classeapprenant` (`idClasseAppenant`, `idClasse`, `idApprenant`) VALUES
-(1, 15, 4),
-(2, 15, 2),
-(3, 15, 1),
-(4, 15, 3),
-(10, 14, 16),
-(11, 14, 17),
-(12, 14, 17),
-(13, 18, 19),
-(14, 3, 20);
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -123,23 +93,23 @@ CREATE TABLE IF NOT EXISTS `classes` (
 
 INSERT INTO `classes` (`idClasse`, `intitule`, `reference`, `annee`, `formation`, `views`) VALUES
 (1, 'Crèche', 1, '2022-2023', 'Maternelle', '2023-02-15 19:45:07'),
-(2, 'Très Petite Section', 2, '2022-2023', 'Maternelle', '2023-02-15 19:45:07'),
-(3, 'Moyenne Section', 3, '2022-2023', 'Maternelle', '2023-02-16 10:08:01'),
-(4, 'Grande Section', 3, '2022-2023', 'Maternelle', '2023-02-15 19:45:07'),
-(5, 'CI', 4, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
-(6, 'CP', 4, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
-(7, 'CE1', 5, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
-(8, 'CE2', 5, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
-(9, 'CM1', 6, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
-(10, 'CM2', 6, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
-(11, 'Petite Section', 3, '2022/2023', 'Maternelle', '2023-02-15 19:45:07'),
-(12, '6eme', 7, '2022/2023', 'College', '2023-02-15 20:22:29'),
+(2, 'Très Petite Section', 2, '2022-2023', 'Maternelle', '2023-02-18 21:02:54'),
+(3, 'Petite Section', 3, '2022/2023', 'Maternelle', '2023-02-15 19:45:07'),
+(4, 'Moyenne Section', 3, '2022-2023', 'Maternelle', '2023-02-18 21:02:48'),
+(5, 'Grande Section', 3, '2022-2023', 'Maternelle', '2023-02-15 19:45:07'),
+(6, 'CI', 4, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
+(7, 'CP', 4, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
+(8, 'CE1', 5, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
+(9, 'CE2', 5, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
+(10, 'CM1', 6, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
+(11, 'CM2', 6, '2022/2023', 'Elementaire', '2023-02-15 19:45:07'),
+(12, '6eme', 7, '2022/2023', 'College', '2023-02-18 21:02:50'),
 (13, '5eme', 7, '2022/2023', 'College', '2023-02-15 19:45:07'),
-(14, '4eme', 8, '2022/2023', 'College', '2023-02-16 08:13:29'),
-(15, '3eme', 8, '2022/2023', 'College', '2023-02-16 08:13:41'),
+(14, '4eme', 8, '2022/2023', 'College', '2023-02-18 21:02:55'),
+(15, '3eme', 8, '2022/2023', 'College', '2023-02-18 21:09:56'),
 (16, '1ere année', 9, '2022/2023', 'Froid/Climatisation', '2023-02-15 19:45:07'),
 (17, '2e année', 9, '2022/2023', 'Froid/Climatisation', '2023-02-15 19:45:07'),
-(18, '3e année', 9, '2022/2023', 'Froid/Climatisation', '2023-02-16 08:15:27');
+(18, '3e année', 9, '2022/2023', 'Froid/Climatisation', '2023-02-18 21:03:00');
 
 -- --------------------------------------------------------
 
@@ -149,12 +119,190 @@ INSERT INTO `classes` (`idClasse`, `intitule`, `reference`, `annee`, `formation`
 
 DROP TABLE IF EXISTS `echeancier`;
 CREATE TABLE IF NOT EXISTS `echeancier` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `intitule` varchar(40) NOT NULL,
-  `date` datetime NOT NULL,
-  `reference` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `idEcheancier` int(11) NOT NULL AUTO_INCREMENT,
+  `intitule` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `idClasse` int(11) NOT NULL,
+  PRIMARY KEY (`idEcheancier`),
+  KEY `ref_classe_rk` (`idClasse`)
+) ENGINE=InnoDB AUTO_INCREMENT=172 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `echeancier`
+--
+
+INSERT INTO `echeancier` (`idEcheancier`, `intitule`, `date`, `idClasse`) VALUES
+(1, 'octobre', '2022-10-01', 1),
+(2, 'novembre', '2022-11-01', 1),
+(3, 'decembre', '2022-12-01', 1),
+(4, 'janvier', '2023-01-01', 1),
+(5, 'fevrier', '2023-02-01', 1),
+(6, 'mars', '2023-02-01', 1),
+(7, 'avril', '2023-02-01', 1),
+(8, 'mai', '2023-02-01', 1),
+(9, 'juin', '2023-02-01', 1),
+(10, 'octobre', '2022-10-01', 2),
+(11, 'novembre', '2022-11-01', 2),
+(12, 'decembre', '2022-12-01', 2),
+(13, 'janvier', '2023-01-01', 2),
+(14, 'fevrier', '2023-02-01', 2),
+(15, 'mars', '2023-02-01', 2),
+(16, 'avril', '2023-02-01', 2),
+(17, 'mai', '2023-02-01', 2),
+(18, 'juin', '2023-02-01', 2),
+(19, 'octobre', '2022-10-01', 3),
+(20, 'novembre', '2022-11-01', 3),
+(21, 'decembre', '2022-12-01', 3),
+(22, 'janvier', '2023-01-01', 3),
+(23, 'fevrier', '2023-02-01', 3),
+(24, 'mars', '2023-02-01', 3),
+(25, 'avril', '2023-02-01', 3),
+(26, 'mai', '2023-02-01', 3),
+(27, 'juin', '2023-02-01', 3),
+(28, 'octobre', '2022-10-01', 4),
+(29, 'novembre', '2022-11-01', 4),
+(30, 'decembre', '2022-12-01', 4),
+(31, 'janvier', '2023-01-01', 4),
+(32, 'fevrier', '2023-02-01', 4),
+(33, 'mars', '2023-02-01', 4),
+(34, 'avril', '2023-02-01', 4),
+(35, 'mai', '2023-02-01', 4),
+(36, 'juin', '2023-02-01', 4),
+(37, 'octobre', '2022-10-01', 5),
+(38, 'novembre', '2022-11-01', 5),
+(39, 'decembre', '2022-12-01', 5),
+(40, 'janvier', '2023-01-01', 5),
+(41, 'fevrier', '2023-02-01', 5),
+(42, 'mars', '2023-02-01', 5),
+(43, 'avril', '2023-02-01', 5),
+(44, 'mai', '2023-02-01', 5),
+(45, 'juin', '2023-02-01', 5),
+(46, 'octobre', '2022-10-01', 5),
+(47, 'novembre', '2022-11-01', 5),
+(48, 'decembre', '2022-12-01', 5),
+(49, 'janvier', '2023-01-01', 5),
+(50, 'fevrier', '2023-02-01', 5),
+(51, 'mars', '2023-02-01', 5),
+(52, 'avril', '2023-02-01', 5),
+(53, 'mai', '2023-02-01', 5),
+(54, 'juin', '2023-02-01', 5),
+(55, 'octobre', '2022-10-01', 6),
+(56, 'novembre', '2022-11-01', 6),
+(57, 'decembre', '2022-12-01', 6),
+(58, 'janvier', '2023-01-01', 6),
+(59, 'fevrier', '2023-02-01', 6),
+(60, 'mars', '2023-02-01', 6),
+(61, 'avril', '2023-02-01', 6),
+(62, 'mai', '2023-02-01', 6),
+(63, 'juin', '2023-02-01', 6),
+(64, 'octobre', '2022-10-01', 7),
+(65, 'novembre', '2022-11-01', 7),
+(66, 'decembre', '2022-12-01', 7),
+(67, 'janvier', '2023-01-01', 7),
+(68, 'fevrier', '2023-02-01', 7),
+(69, 'mars', '2023-02-01', 7),
+(70, 'avril', '2023-02-01', 7),
+(71, 'mai', '2023-02-01', 7),
+(72, 'juin', '2023-02-01', 7),
+(73, 'octobre', '2022-10-01', 8),
+(74, 'novembre', '2022-11-01', 8),
+(75, 'decembre', '2022-12-01', 8),
+(76, 'janvier', '2023-01-01', 8),
+(77, 'fevrier', '2023-02-01', 8),
+(78, 'mars', '2023-02-01', 8),
+(79, 'avril', '2023-02-01', 8),
+(80, 'mai', '2023-02-01', 8),
+(81, 'juin', '2023-02-01', 8),
+(82, 'octobre', '2022-10-01', 9),
+(83, 'novembre', '2022-11-01', 9),
+(84, 'decembre', '2022-12-01', 9),
+(85, 'janvier', '2023-01-01', 9),
+(86, 'fevrier', '2023-02-01', 9),
+(87, 'mars', '2023-02-01', 9),
+(88, 'avril', '2023-02-01', 9),
+(89, 'mai', '2023-02-01', 9),
+(90, 'juin', '2023-02-01', 9),
+(91, 'octobre', '2022-10-01', 10),
+(92, 'novembre', '2022-11-01', 10),
+(93, 'decembre', '2022-12-01', 10),
+(94, 'janvier', '2023-01-01', 10),
+(95, 'fevrier', '2023-02-01', 10),
+(96, 'mars', '2023-02-01', 10),
+(97, 'avril', '2023-02-01', 10),
+(98, 'mai', '2023-02-01', 10),
+(99, 'juin', '2023-02-01', 10),
+(100, 'octobre', '2022-10-01', 11),
+(101, 'novembre', '2022-11-01', 11),
+(102, 'decembre', '2022-12-01', 11),
+(103, 'janvier', '2023-01-01', 11),
+(104, 'fevrier', '2023-02-01', 11),
+(105, 'mars', '2023-02-01', 11),
+(106, 'avril', '2023-02-01', 11),
+(107, 'mai', '2023-02-01', 11),
+(108, 'juin', '2023-02-01', 11),
+(109, 'octobre', '2022-10-01', 12),
+(110, 'novembre', '2022-11-01', 12),
+(111, 'decembre', '2022-12-01', 12),
+(112, 'janvier', '2023-01-01', 12),
+(113, 'fevrier', '2023-02-01', 12),
+(114, 'mars', '2023-02-01', 12),
+(115, 'avril', '2023-02-01', 12),
+(116, 'mai', '2023-02-01', 12),
+(117, 'juin', '2023-02-01', 12),
+(118, 'octobre', '2022-10-01', 13),
+(119, 'novembre', '2022-11-01', 13),
+(120, 'decembre', '2022-12-01', 13),
+(121, 'janvier', '2023-01-01', 13),
+(122, 'fevrier', '2023-02-01', 13),
+(123, 'mars', '2023-02-01', 13),
+(124, 'avril', '2023-02-01', 13),
+(125, 'mai', '2023-02-01', 13),
+(126, 'juin', '2023-02-01', 13),
+(127, 'octobre', '2022-10-01', 14),
+(128, 'novembre', '2022-11-01', 14),
+(129, 'decembre', '2022-12-01', 14),
+(130, 'janvier', '2023-01-01', 14),
+(131, 'fevrier', '2023-02-01', 14),
+(132, 'mars', '2023-02-01', 14),
+(133, 'avril', '2023-02-01', 14),
+(134, 'mai', '2023-02-01', 14),
+(135, 'juin', '2023-02-01', 14),
+(136, 'octobre', '2022-10-01', 15),
+(137, 'novembre', '2022-11-01', 15),
+(138, 'decembre', '2022-12-01', 15),
+(139, 'janvier', '2023-01-01', 15),
+(140, 'fevrier', '2023-02-01', 15),
+(141, 'mars', '2023-02-01', 15),
+(142, 'avril', '2023-02-01', 15),
+(143, 'mai', '2023-02-01', 15),
+(144, 'juin', '2023-02-01', 15),
+(145, 'octobre', '2022-10-01', 16),
+(146, 'novembre', '2022-11-01', 16),
+(147, 'decembre', '2022-12-01', 16),
+(148, 'janvier', '2023-01-01', 16),
+(149, 'fevrier', '2023-02-01', 16),
+(150, 'mars', '2023-02-01', 16),
+(151, 'avril', '2023-02-01', 16),
+(152, 'mai', '2023-02-01', 16),
+(153, 'juin', '2023-02-01', 16),
+(154, 'octobre', '2022-10-01', 17),
+(155, 'novembre', '2022-11-01', 17),
+(156, 'decembre', '2022-12-01', 17),
+(157, 'janvier', '2023-01-01', 17),
+(158, 'fevrier', '2023-02-01', 17),
+(159, 'mars', '2023-02-01', 17),
+(160, 'avril', '2023-02-01', 17),
+(161, 'mai', '2023-02-01', 17),
+(162, 'juin', '2023-02-01', 17),
+(163, 'octobre', '2022-10-01', 18),
+(164, 'novembre', '2022-11-01', 18),
+(165, 'decembre', '2022-12-01', 18),
+(166, 'janvier', '2023-01-01', 18),
+(167, 'fevrier', '2023-02-01', 18),
+(168, 'mars', '2023-02-01', 18),
+(169, 'avril', '2023-02-01', 18),
+(170, 'mai', '2023-02-01', 18),
+(171, 'juin', '2023-02-01', 18);
 
 -- --------------------------------------------------------
 
@@ -170,7 +318,7 @@ CREATE TABLE IF NOT EXISTS `modules` (
   `semestre` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`idModule`),
   KEY `idClasse` (`idClasse`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `modules`
@@ -181,7 +329,128 @@ INSERT INTO `modules` (`idModule`, `intitule`, `idClasse`, `semestre`) VALUES
 (12, 'Français', 15, 1),
 (13, 'Histo/Geo', 15, 1),
 (14, 'Physique-Chimie', 15, 1),
-(15, 'Science de la vie et de la terre', 15, 1);
+(15, 'Science de la vie et de la terre', 15, 1),
+(16, 'Education physique', 15, 1),
+(17, 'Anglais', 15, 1),
+(18, 'Maths', 12, 1),
+(19, 'Histo/Geo', 12, 1),
+(20, 'Anglais', 12, 1),
+(21, 'Science de la vie et de la terre', 12, 1),
+(22, 'Education physique', 12, 1),
+(23, 'Physique-Chimie', 12, 1),
+(24, 'Français', 12, 1),
+(25, 'Maths', 12, 2),
+(26, 'Histo/Geo', 12, 2),
+(27, 'Anglais', 12, 2),
+(28, 'Science de la vie et de la terre', 12, 2),
+(29, 'Education physique', 12, 2),
+(30, 'Physique-Chimie', 12, 2),
+(31, 'Français', 12, 2),
+(32, 'Maths', 15, 2),
+(33, 'Histo/Geo', 15, 2),
+(34, 'Anglais', 15, 2),
+(35, 'Science de la vie et de la terre', 15, 2),
+(36, 'Education physique', 15, 2),
+(37, 'Physique-Chimie', 15, 2),
+(38, 'Français', 15, 2),
+(39, 'Maths', 13, 2),
+(40, 'Histo/Geo', 13, 2),
+(41, 'Anglais', 13, 2),
+(42, 'Science de la vie et de la terre', 13, 2),
+(43, 'Education physique', 13, 2),
+(44, 'Physique-Chimie', 13, 2),
+(45, 'Français', 13, 2),
+(46, 'Maths', 13, 1),
+(47, 'Histo/Geo', 13, 1),
+(48, 'Anglais', 13, 1),
+(49, 'Science de la vie et de la terre', 13, 1),
+(50, 'Education physique', 13, 1),
+(51, 'Physique-Chimie', 13, 1),
+(52, 'Français', 13, 1),
+(53, 'Maths', 14, 2),
+(54, 'Histo/Geo', 14, 2),
+(55, 'Anglais', 14, 2),
+(56, 'Science de la vie et de la terre', 14, 2),
+(57, 'Education physique', 14, 2),
+(58, 'Physique-Chimie', 14, 2),
+(59, 'Français', 14, 2),
+(60, 'Maths', 14, 1),
+(61, 'Histo/Geo', 14, 1),
+(62, 'Anglais', 14, 1),
+(63, 'Science de la vie et de la terre', 14, 1),
+(64, 'Education physique', 14, 1),
+(65, 'Physique-Chimie', 14, 1),
+(66, 'Français', 14, 1),
+(81, 'Maths', 7, 1),
+(82, 'Français', 7, 1),
+(83, 'Sciences', 7, 1),
+(84, 'Histo/Geo', 7, 1),
+(85, 'Savoir vivre', 7, 1),
+(86, 'Education artistique', 7, 1),
+(87, 'Anglais', 7, 1),
+(88, 'Maths', 7, 2),
+(89, 'Français', 7, 2),
+(90, 'Sciences', 7, 2),
+(91, 'Histo/Geo', 7, 2),
+(92, 'Savoir vivre', 7, 2),
+(93, 'Education artistique', 7, 2),
+(94, 'Anglais', 7, 2),
+(95, 'Maths', 8, 1),
+(96, 'Français', 8, 1),
+(97, 'Sciences', 8, 1),
+(98, 'Histo/Geo', 8, 1),
+(99, 'Savoir vivre', 8, 1),
+(100, 'Education artistique', 8, 1),
+(101, 'Anglais', 8, 1),
+(102, 'Maths', 8, 2),
+(103, 'Français', 8, 2),
+(104, 'Sciences', 8, 2),
+(105, 'Histo/Geo', 8, 2),
+(106, 'Savoir vivre', 8, 2),
+(107, 'Education artistique', 8, 2),
+(108, 'Anglais', 8, 2),
+(109, 'Maths', 9, 1),
+(110, 'Français', 9, 1),
+(111, 'Sciences', 9, 1),
+(112, 'Histo/Geo', 9, 1),
+(113, 'Savoir vivre', 9, 1),
+(114, 'Education artistique', 9, 1),
+(115, 'Anglais', 9, 1),
+(116, 'Maths', 9, 2),
+(117, 'Français', 9, 2),
+(118, 'Sciences', 9, 2),
+(119, 'Histo/Geo', 9, 2),
+(120, 'Savoir vivre', 9, 2),
+(121, 'Education artistique', 9, 2),
+(122, 'Anglais', 9, 2),
+(123, 'Maths', 10, 1),
+(124, 'Français', 10, 1),
+(125, 'Sciences', 10, 1),
+(126, 'Histo/Geo', 10, 1),
+(127, 'Savoir vivre', 10, 1),
+(128, 'Education artistique', 10, 1),
+(129, 'Anglais', 10, 1),
+(130, 'Maths', 10, 2),
+(131, 'Français', 10, 2),
+(132, 'Sciences', 10, 2),
+(133, 'Histo/Geo', 10, 2),
+(134, 'Savoir vivre', 10, 2),
+(135, 'Education artistique', 10, 2),
+(136, 'Anglais', 10, 2),
+(137, 'Maths', 11, 1),
+(138, 'Français', 11, 1),
+(139, 'Sciences', 11, 1),
+(140, 'Histo/Geo', 11, 1),
+(141, 'Savoir vivre', 11, 1),
+(142, 'Education artistique', 11, 1),
+(143, 'Anglais', 11, 1),
+(144, 'Maths', 11, 2),
+(145, 'Français', 11, 2),
+(146, 'Sciences', 11, 2),
+(147, 'Histo/Geo', 11, 2),
+(148, 'Savoir vivre', 11, 2),
+(149, 'Education artistique', 11, 2),
+(150, 'Anglais', 11, 2);
 
 -- --------------------------------------------------------
 
@@ -198,21 +467,7 @@ CREATE TABLE IF NOT EXISTS `notes` (
   PRIMARY KEY (`idNote`),
   KEY `idModule` (`idModule`),
   KEY `idApprenant` (`idApprenant`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `notes`
---
-
-INSERT INTO `notes` (`idNote`, `valeur`, `idApprenant`, `idModule`) VALUES
-(1, 14, 3, 14),
-(2, 18, 4, 14),
-(3, 10, 1, 14),
-(4, 20, 2, 14),
-(5, 9, 4, 11),
-(6, 13, 1, 11),
-(7, 18, 3, 11),
-(8, 10, 4, 11);
+) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -315,6 +570,12 @@ INSERT INTO `utilisateurs` (`idUtilisateur`, `password`, `nom`, `prenom`, `email
 --
 ALTER TABLE `classeapprenant`
   ADD CONSTRAINT `classeapprenant_ibfk_2` FOREIGN KEY (`idClasse`) REFERENCES `classes` (`idClasse`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `echeancier`
+--
+ALTER TABLE `echeancier`
+  ADD CONSTRAINT `ref_classe_rk` FOREIGN KEY (`idClasse`) REFERENCES `classes` (`idClasse`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `modules`
